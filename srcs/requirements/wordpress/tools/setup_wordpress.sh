9492abcca1f7f4340c7c49d2e620f64cbaca3db7
nils <nils@myhost.42wolfsburg.de>
2023-01-31 14:07:00 +0000

    some configuration changes in wp and mariadb

--- srcs/requirements/wordpress/tools/setup_wordpress.sh
+++ srcs/requirements/wordpress/tools/setup_wordpress.sh
@@ -2,16 +2,16 @@
 
 if [ ! -d "/var/www/html/wordpress/wp-config.php" ]
 then
-	wget -P /var/www/html https://wordpress.org/latest.tar.gz
-	tar -C /var/www/html -xzvf /var/www/html/latest.tar.gz
-	rm -f /var/www/html/latest.tar.gz
-	chown -R nginx:nginx /var/www/html/wordpress
+	wget https://wordpress.org/latest.tar.gz
+	tar -xzvf /var/www/html/latest.tar.gz
+	rm -f latest.tar.gz
+	chown -R www-data:www-data wordpress
 
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	cp wp-config-sample.php wp-config.php
+	sed -i "s/database_name_here/$MYSQL_DATABASE/g" ./wordpress/wp-config-sample.php
+	sed -i "s/username_here/$MYSQL_USER/g" ./wordpress/wp-config-sample.php
+	sed -i "s/password_here/$MYSQL_PASSWORD/g" ./wordpress/wp-config-sample.php
+	sed -i "s/localhost/$MYSQL_HOSTNAME/g" ./wordpress/wp-config-sample.php
+	cp ./wordpress/wp-config-sample.php ./wordpress/wp-config.php
 fi
 
 exec "$@"
